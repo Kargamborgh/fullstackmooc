b87eb3c824df385a0c5e5e3fbc4dc4b2ab016ddb
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-16 14:13:25 +0300

    1.7 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -14,14 +14,39 @@ const Button = (props) => (
 
 const App = () => {
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad+1)
-
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [sum, setSum] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    setSum(sum + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  } 
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    setSum(sum - 1)
+  }
+
+  const countAverage = (props) => {
+    return (
+      sum / total
+    )
+  }
+
+  const countPositivePercentage = (props) => {
+    return (
+      (good / total)*100
+    )
+  }
+
 
   return (
     <div>
@@ -33,6 +58,9 @@ const App = () => {
       <Display value={good} text='good' />
       <Display value={neutral} text='neutral' />
       <Display value={bad} text='bad' />
+      <Display value={total} text='total' />
+      <Display value={countAverage()} text='average' />
+      <Display value={countPositivePercentage()} text='positive percentage'/>
     </div>
   )
 }
