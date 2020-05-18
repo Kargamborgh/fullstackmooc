099eb4c05899c68fb5d7b01e8684ea8a03c8406a
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-18 19:23:52 +0300

    2.11 tehty

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import Person from './components/Person'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filterValue, setFilterValue ] = useState('')
+
+  const effHook = () => {
+    console.log('effect lol')
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }
+
+  useEffect(effHook, [])
 
   const addName = (event) => {
     event.preventDefault()
@@ -50,8 +57,6 @@ const App = () => {
     console.log(event.target.value)
     setFilterValue(event.target.value)
   }
-
-  
 
   return (
     <div>
