321678338e2ce1761927853d4fd8a8d57acd9e6c
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-24 19:56:46 +0300

    4.4 tehty

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,8 +1,19 @@
 /* eslint-disable linebreak-style */
-const dummy = (blogs) => {
+const dummy = () => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length > 0) {
+    const likesArray = blogs.map(blogs => blogs.likes)
+    const reducer = (sum, current) => sum + current
+    return (likesArray.reduce(reducer))
+  } else {
+    return 0
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
