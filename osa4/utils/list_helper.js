102f88b3be1b7df73d0d81a5a5765ba151766a1c
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-24 21:20:38 +0300

    4.5* tehty

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -13,7 +13,23 @@ const totalLikes = (blogs) => {
   }
 }
 
+const favoriteBlog = (blogs) => {
+  const likesArray = blogs.map(blogs => blogs.likes)
+  const indexOfMostLikes =
+    likesArray
+      .reduce((bestSoFar, currentV, currentI, arr) => currentV > arr[bestSoFar] ? currentI : bestSoFar, 0)
+
+  return (
+    {
+      'title': blogs[indexOfMostLikes].title,
+      'author': blogs[indexOfMostLikes].author,
+      'likes': blogs[indexOfMostLikes].likes
+    }
+  )
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
