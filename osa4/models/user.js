0b6995cb12b9eaf43e884d09ede9dee5c4005401
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-26 19:54:31 +0300

    4.15 tehty

---
+++ osa4/models/user.js
@@ -0,0 +1,26 @@
+/* eslint-disable linebreak-style */
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  passwordHash: String,
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    // the passwordHash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
