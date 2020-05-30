ca845a076b3f4c8c587bdfb41b952ae89b8233b1
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-30 15:56:11 +0300

    5.15 yhä tehty, lisätty backendin tarpeelliset muutokset gittiin

--- osa4/utils/logger.js
+++ osa4/utils/logger.js
@@ -1,8 +1,8 @@
 /* eslint-disable linebreak-style */
 const info = (...params) => {
-  if (process.env.NODE_ENV !== 'test') {
-    console.log(...params)
-  }
+  //if (process.env.NODE_ENV !== 'test') {
+  console.log(...params)
+  //}
 }
 
 const error = (...params) => {
