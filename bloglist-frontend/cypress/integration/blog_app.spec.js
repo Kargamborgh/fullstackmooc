a0bc70b284c52b586d353d7affc7827271098fac
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-05-30 16:09:46 +0300

    5.18 tehty

--- bloglist-frontend/cypress/integration/blog_app.spec.js
+++ bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -2,6 +2,12 @@
 describe('Blog app', function() {
     beforeEach(function() {
       cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      const user = {
+        name: 'Mikki Hiiri',
+        username: 'mhiiri',
+        password: 'salakala'
+      }
+      cy.request('POST', 'http://localhost:3001/api/users/', user)
       cy.visit('http://localhost:3000')
     })
   
@@ -9,5 +15,29 @@ describe('Blog app', function() {
       cy.contains('login').click()
       cy.contains('username')
       cy.contains('password')
+    })
+
+    describe('Login', function() {
+
+        it('succeeds with correct credentials', function() {
+            cy.contains('login').click()
+            cy.get('#username').type('mhiiri')
+            cy.get('#password').type('salakala')
+            cy.get('#login-button').click()
+        
+            cy.contains('Mikki Hiiri logged in')
+        })
+
+        it('fails with wrong credentials', function() {
+            cy.contains('login').click()
+            cy.get('#username').type('mhiiri')
+            cy.get('#password').type('wrongpassword')
+            cy.get('#login-button').click()
+
+            cy.get('.error')
+            .should('contain', 'wrong credentials')
+
+            cy.get('html').should('not.contain', 'Mikki Hiiri logged in')
+        })
     })
   })
