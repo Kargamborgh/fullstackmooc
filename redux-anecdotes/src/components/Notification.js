77941bed9892c497f2d92eec82ff4a05df3f80fb
Kargamborgh <niilo.korkiakoski@gmail.com>
2020-06-02 21:40:33 +0300

    6.11 siistitty, notifikaatioboksi katoaa hideNotificationin yhteydessä style-ehtolauseella

--- redux-anecdotes/src/components/Notification.js
+++ redux-anecdotes/src/components/Notification.js
@@ -5,13 +5,19 @@ import { useSelector } from 'react-redux'
 const Notification = () => {
   const notification = useSelector(state => state.notification)
 
+  
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1
   }
+
+  const style2 = {
+    padding: 10
+  }
+  
   return (
-    <div style={style}>
+    <div style={notification !== '' ? style : style2}>
       {notification}
     </div>
   )
